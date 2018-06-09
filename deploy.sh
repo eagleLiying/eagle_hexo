79d8ed94a25d1111451c3a9ef02b12d4b82a051d
lyly312312 <zwly@zhiwen.me>
2018-06-09 21:43:25 +0800

    修改配置

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
+mkdir .deploy
+cd .deploy
 # 克隆
 git clone https://github.com/eagleLiying/eagleLiying.github.io.git
-
+cd ..
 hexo generate
-cp -R public/* eagleLiying.github.io
-cd eagleLiying.github.io
+cp -R public/* .deploy/eagleLiying.github.io
+cd  .deploy/eagleLiying.github.io
 git add .
 git commit -m “update blog”
 git push origin master
